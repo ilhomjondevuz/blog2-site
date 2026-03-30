e5f8ab06dc37f0b17b719b0c5d9e6ca0a60f65ac
Ilhomjon <rahimovilhomjon25@gmail.com>
2026-03-30 16:17:21 +0500

    autorizatsiya qo'shildi

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
@@ -16,19 +16,28 @@ class ArticleDetailView(DetailView):
     template_name = 'blogs/article_detail.html'
     context_object_name = 'article'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Article
-    fields = ['title', 'author', 'summary', 'content', 'photo', ]
-    success_url = reverse_lazy('home')
+    fields = ['title', 'summary', 'content', 'photo', ]
     template_name = 'blogs/article_create.html'
     context_object_name = 'article'
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_create'] = False
         return context
 
-class ArticleDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'blogs/article_delete.html'
     context_object_name = 'article'
@@ -39,13 +48,25 @@ class ArticleDeleteView(DeleteView):
         context['is_delete'] = False
         return context
 
-class ArticleUpdateView(UpdateView):
+    def test_func(self):
+        article = self.get_object()
+        return article.author == self.request.user
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
-    fields = ['title', 'author', 'summary', 'content', 'photo', ]
-    success_url = reverse_lazy('home')
+    fields = ['title', 'summary', 'content', 'photo']
     template_name = 'blogs/article_create.html'
+    context_object_name = 'article'
+
+    def get_success_url(self):
+        return reverse_lazy('article_detail', kwargs={'pk': self.object.pk})
+
+    def test_func(self):
+        # Faqat author o‘zgartira oladi
+        article = self.get_object()
+        return article.author == self.request.user
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['is_update'] = False
+        context['is_update'] = True
         return context
